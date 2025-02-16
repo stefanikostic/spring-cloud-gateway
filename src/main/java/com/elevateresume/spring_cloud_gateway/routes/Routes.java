75708a8983901c70f0d897348c5a2c648cab0585
Stefani Kostic <stefanikostic6@gmail.com>
2025-02-16 22:57:41 +0100

    Added route methods for user and resume service

--- src/main/java/com/elevateresume/spring_cloud_gateway/routes/Routes.java
+++ src/main/java/com/elevateresume/spring_cloud_gateway/routes/Routes.java
@@ -1,20 +1,26 @@
 package com.elevateresume.spring_cloud_gateway.routes;
 
+import com.elevateresume.spring_cloud_gateway.security.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
 import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.RouterFunctions;
 import org.springframework.web.servlet.function.ServerResponse;
 
 @Configuration
 public class Routes {
 
-   /* @Bean
+    @Bean
     public RouterFunction<ServerResponse> authService() {
-        return GatewayRouterFunctions.route("user-service")
-                .route(RequestPredicates.path("/auth/**"), HandlerFunctions.http("http://localhost:8081/"))
-                .build();
-    }*/
+        return GatewayRouterFunctions.route("user-service").route(RequestPredicates.path("/auth/**"), HandlerFunctions.http("http://localhost:8081/")).build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> resumeService() {
+        return RouterFunctions.route(RequestPredicates.path("/resume/**"), request -> HandlerFunctions.http("http://localhost:8082/").handle(request));
+    }
 }
